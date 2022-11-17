7bf312093a2a00a200d762cb8ffdbf40f3f2ba9c
ChristianSpeziali <spezialichristian@gmail.com>
2022-11-17 10:18:04 -0300

    navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,23 @@
 import "./App.css";
 import About from './components/About/About'
 import Contact from './components/Contact/Contact'
+import Footer from './components/Footer/Footer'
+import Header from './components/Header/Header'
+
 
 function App() {
   return (
     <div className="App">
       <h1>Hola</h1>
+      <Header />
       <main>
         <About />
         {/* <Projects />
         <Skills /> */}
         <Contact />
       </main>
+      {/* <ScrollToTop /> */}
+      <Footer />
     </div>
   );
 }
